8368b43f5fdf2b59028ab77b850e17738d704907
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2015-03-21 15:00:57 -0400

    - Welcome page transition is working

--- app/src/main/java/com/handshake/handshake/ChooseFairActivity.java
+++ app/src/main/java/com/handshake/handshake/ChooseFairActivity.java
@@ -1,26 +1,79 @@
 package com.handshake.handshake;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class ChooseFairActivity extends Activity {
-
+    ListView listView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_fair);
+
+        // Get ListView object from xml
+        listView = (ListView) findViewById(R.id.list);
+
+        // Defined Array values to show in ListView
+        String[] values = new String[] { "Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View",
+                "Hari",
+                "Jagan",
+                "Javi",
+                "Rrahul",
+                "Potato",
+                "Banana",
+                "Apple",
+                "Vijay",
+                "Thuss"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // ListView Item Click Listener
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+//                ListEntry entry= (ListEntry) parent.getAdapter().getItem(position);
+                Intent intent = new Intent(ChooseFairActivity.this, SelectUserTypeActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_choose_fair, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
