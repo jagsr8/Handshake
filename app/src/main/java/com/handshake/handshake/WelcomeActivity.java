f8432e1491f8433f7f7f615f8e1499ec3a86443f
Jagan Srini <jagsr8@gmail.com>
2015-03-21 16:01:04 -0400

    Parse update

--- app/src/main/java/com/handshake/handshake/WelcomeActivity.java
+++ app/src/main/java/com/handshake/handshake/WelcomeActivity.java
@@ -7,11 +7,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 public class WelcomeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "Aw2wgeVa9ihzU3OqRggicJ2rcFoQFxQYIWDhnaFR", "sgADHHUmzraH6rkEoskMeKpjvkHSq9z0DQWBgZJw");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
 
         setContentView(R.layout.activity_welcome);
     }
