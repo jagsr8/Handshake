eb640998d94e64f69feb926e29d53e17f93fc972
Ganapathy Hari Narayan <ganapathy.hari.narayan@gatech.edu>
2015-03-21 23:44:11 -0400

    - Welcome page transition is working

--- app/src/main/java/com/handshake/handshake/SelectUserTypeActivity.java
+++ app/src/main/java/com/handshake/handshake/SelectUserTypeActivity.java
@@ -1,33 +1,30 @@
 package com.handshake.handshake;
 
+import com.handshake.handshake.util.SystemUiHider;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-import com.handshake.handshake.util.SystemUiHider;
 
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class SelectUserTypeActivity extends Activity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_select_user_type);
         Intent intent = getIntent();
         String message = intent.getStringExtra(ChooseFairActivity.EXTRA_MESSAGE);
-        TextView textView = new TextView(this);
+        TextView textView = (TextView) findViewById(R.id.textView);
         textView.setTextSize(40);
         textView.setText(message);
-        setContentView(textView);
     }
 
     public void chooseStudent(View view) {
