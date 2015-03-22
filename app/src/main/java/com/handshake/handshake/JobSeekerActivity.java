9546f185d6df0c2d23cf33ff03c26d76239da95a
unknown <javirod77@live.com>
2015-03-22 01:22:00 -0400

    Starting Myo

--- app/src/main/java/com/handshake/handshake/JobSeekerActivity.java
+++ app/src/main/java/com/handshake/handshake/JobSeekerActivity.java
@@ -1,21 +1,53 @@
 package com.handshake.handshake;
 
-import com.handshake.handshake.util.SystemUiHider;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.Toast;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.Pose;
 
 public class JobSeekerActivity extends Activity {
 
+    private Intent intent; // gets the Myo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_job_seeker);
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this)) {
+            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        Hub.getInstance().attachToAdjacentMyo();
+        Hub.getInstance().setLockingPolicy(Hub.LockingPolicy.STANDARD);
     }
 
-    public void sendFile(View view) {
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            System.out.println("5");
+            //Toast.makeText(mContext, "Myo Connected!", Toast.LENGTH_SHORT).show();
+        }
 
-    }
+        @Override
+        public void onDisconnect(Myo myo, long timestamp) {
+            //Toast.makeText(mContext, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPose(Myo myo, long timestamp, Pose pose) {
+            System.out.println("heyyy");
+            //Toast.makeText(mContext, "Pose: " + pose, Toast.LENGTH_SHORT).show();
+
+            //TODO: Do something awesome.
+        }
+    };
 }
